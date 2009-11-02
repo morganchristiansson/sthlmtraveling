256dfe29d94bf3da44a096d738e523cf6cf89567
Morgan Christiansson <morgan@macmini.(none)>
2009-11-02 21:53:49 +0100

    Restore onActivityResult() handler so that sub-activities correctly return their results.

--- src/com/markupartist/sthlmtraveling/PlannerActivity.java
+++ src/com/markupartist/sthlmtraveling/PlannerActivity.java
@@ -24,6 +24,7 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.format.Time;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -125,7 +126,23 @@ public class PlannerActivity extends Activity implements OnSearchRoutesResultLis
             startActivityForResult(new Intent(PlannerActivity.this, ToActivity.class), ACTIVITY_TO);
         }
     };
-
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode != RESULT_OK) return;
+        switch(requestCode) {
+        case ACTIVITY_FROM:
+            mFromButton.setText(data.getCharSequenceExtra("com.markupartist.sthlmtraveling.startPoint"));
+            break;
+        case ACTIVITY_TO:
+            mToButton.setText(data.getCharSequenceExtra("com.markupartist.sthlmtraveling.endPoint"));
+            break;
+        case ACTIVITY_WHEN:
+            mWhenButton.setText(data.getCharSequenceExtra("com.markupartist.sthlmtraveling.routeTime"));
+            break;
+        default:
+            Log.w(TAG, "Unhandled activity resultCode: "+resultCode);
+        }
+    }
     @Override
     protected Dialog onCreateDialog(int id) {
         Dialog dialog = null;
