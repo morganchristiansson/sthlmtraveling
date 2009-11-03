0677d322393a61217a1c7f5c27ca57f60d5dcfda
Morgan Christiansson <morgan@macmini.(none)>
2009-11-03 16:28:30 +0100

    Disable unsupported components in WhenActivity.

--- src/com/markupartist/sthlmtraveling/WhenActivity.java
+++ src/com/markupartist/sthlmtraveling/WhenActivity.java
@@ -6,6 +6,8 @@ import java.util.Calendar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -13,9 +15,8 @@ import android.widget.Spinner;
 import android.widget.TimePicker;
 
 public class WhenActivity extends Activity {
-	static final int TIME_DIALOG_ID = 0;
 	private TimePicker mTime;
-    private Calendar mCalendar;
+    //private Calendar mCalendar;
     private Button mGo;
 
 	@Override
@@ -25,41 +26,44 @@ public class WhenActivity extends Activity {
 
         mTime = (TimePicker)findViewById(R.id.time);
         mTime.setIs24HourView(true);
-
+        /*
         mCalendar = Calendar.getInstance();
 
 		Spinner date = (Spinner)findViewById(R.id.date);
 		ArrayAdapter<CharSequence> dateAdapter = new ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_item, prepopulateCalendar());
 		dateAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		date.setAdapter(dateAdapter);
+        */
 
-		mGo = (Button)findViewById(R.id.go);
+        mGo = (Button)findViewById(R.id.go);
 		mGo.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				Intent i = new Intent();
-				i.putExtra("com.markupartist.sthlmtraveling.routeTime",
-				           ""+mTime.getCurrentHour()+":"+mTime.getCurrentMinute());
+                i.putExtra("com.markupartist.sthlmtraveling.routeTime",
+                        ""+mTime.getCurrentHour()+":"+mTime.getCurrentMinute());
 				setResult(RESULT_OK, i);
 				finish();
 			}
 		});
 	}
 
-	private String[] prepopulateCalendar() {
-		SimpleDateFormat format = new SimpleDateFormat("dd MMMM EEEE");
+	/*
+	private Spanned[] prepopulateCalendar() {
+		SimpleDateFormat format = new SimpleDateFormat("'<b>'dd'</b>' MMMM EEEE");
 		SimpleDateFormat weekdayFormat = new SimpleDateFormat("EEEE");
 		mCalendar = Calendar.getInstance();
-		String dates[] = new String[30];
-		dates[0] = getText(R.string.today)+" "+weekdayFormat.format(mCalendar.getTime());
+		Spanned dates[] = new Spanned[30];
+		dates[0] = Html.fromHtml("<b>"+getText(R.string.today)+"</b> "+weekdayFormat.format(mCalendar.getTime()));
 		mCalendar.add(Calendar.DAY_OF_MONTH, 1);
-		dates[1] = getText(R.string.tomorrow)+" "+weekdayFormat.format(mCalendar.getTime());
+		dates[1] = Html.fromHtml("<b>"+getText(R.string.tomorrow)+"</b> "+weekdayFormat.format(mCalendar.getTime()));
 		mCalendar.add(Calendar.DAY_OF_MONTH, 1);
 		
 		for(int i=2; i<30; i++) {
-			dates[i] = format.format(mCalendar.getTime());
+			dates[i] = Html.fromHtml(format.format(mCalendar.getTime()));
 			mCalendar.add(Calendar.DAY_OF_MONTH, 1);
 		}
 		return dates;
 	}
+	*/
 }
